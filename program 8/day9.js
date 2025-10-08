91c451bbe738f585c196e6e9c4cbfdc6381d6ff3
prajith-bug <prajith1817@gmail.com>
2025-10-08 11:17:43 +0530

    Add files via upload

---
+++ program 8/day9.js
@@ -0,0 +1,9 @@
+//Import and Export Modules
+
+//Definition:
+//Export allows you to make variables,functions,or
+//classes available to other files.
+
+//Import allows you to bring those into another file.
+
+//used in modular JavaScript(ES6+).
